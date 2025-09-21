64380503e036149af302f49d40c566dc775f1ba9
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-09-21 20:56:32 +0800

    setup: database

---
+++ db/populatedb.js
@@ -0,0 +1,34 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        firstname VARCHAR (255),
+        lastname VARCHAR (255),
+        username VARCHAR (255),
+        password VARCHAR (255),
+        is_admin BOOLEAN,
+        is_member BOOLEAN
+    );
+
+    CREATE TABLE IF NOT EXISTS messages (
+        message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        message VARCHAR (255),
+        timestamp VARCHAR (255)
+    );
+`
+async function main() {
+    console.log("seeding");
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+    });
+
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+
+    console.log("done");
+};
+
+main();
