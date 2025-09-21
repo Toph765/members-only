aeced9e8a7d23e7750a5d91416a23be3415e3363
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-09-21 20:53:59 +0800

    setup: dependencies

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('node:path');
 const assetsPath = path.join(__dirname, 'public');
+const pool = require("./db/pool");
+const session = require("express-session");
+const passport = require("passport");
+const pgSession = require("connect-pg-simple")(session);
+require("dotenv").config();
+require("./lib/passport");
+
+const sessionStore = new pgSession({ pool });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('views engine', 'ejs');
-app.set(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
