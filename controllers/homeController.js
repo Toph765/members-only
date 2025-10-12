611944665fd4d9041f157c448380e3a93878fec0
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-10-12 18:24:05 +0800

    feat: display home page; create, display messages

---
+++ controllers/homeController.js
@@ -0,0 +1,34 @@
+const db = require("../db/queries");
+const { format } = require("date-fns");
+
+async function getHomePage(req, res) {
+    const messages = await db.getMessages();
+    console.log(messages);
+
+    res.render("homepage", {user: req.user});
+};
+
+function getNewMessage(req, res) {
+    res.render("newMessage");
+}
+
+async function postNewMessage(req, res) {
+    const { message } = req.body;
+    const { user_id } = req.user;
+    const timestamp = format(new Date(), "PP',' p");
+
+    const messageDetails = {
+        message: message,
+        timestamp: timestamp,
+        userId: user_id,
+    }
+
+    await db.addNewMessage(messageDetails);
+
+    res.redirect("/home");
+}
+module.exports = {
+    getHomePage,
+    getNewMessage,
+    postNewMessage
+}
